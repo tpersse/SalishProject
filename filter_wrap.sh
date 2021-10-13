13bfccaddcbdc41dccec034fc9cc0cb88cd85703
Thomas Persse <tpersse@talapas-ln1.cm.cluster>
2021-10-12 22:22:59 -0700

    working filtering script10_12_2021

---
+++ filter_wrap.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --account=bgmp
+#SBATCH --partition=bgmp
+#SBATCH --cpus-per-task=8
+#SBATCH --nodes=1
+#SBATCH --time=10:00:00
+#SBATCH --output=../filter%j.out
+#SBATCH --error=../filter%j.err
+#SBATCH --mail-user='tpersse@uoregon.edu'
+#SBATCH --mail-type=END,FAIL
+
+conda activate bgmp_py39
+
+/usr/bin/time -v python filter_2.py --linda ./../lindas.txt --probe ./../target_oligs.txt --read1 ./../Project-35_S1_L001_R1_001.fastq --read2 ./../Project-35_S1_L001_R2_001.fastq
